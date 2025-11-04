352917806305c4b983d1d5d418e77d08e001d54e
Samarth Choksi <choksisamarth2@gmail.com>
2025-11-04 10:57:43 +0530

    updated data

--- seedproduct.js
+++ seedproduct.js
@@ -1,24 +1,26 @@
-const mongoose = require('mongoose')
-const { PRODUCT } = require('./models/user')
-const data = require('./PRODUCTS/products')
-require('dotenv').config()
+const mongoose = require('mongoose');
+const { PRODUCT } = require('./models/user');
+const data = require('./PRODUCTS/products');
+require('dotenv').config();
 
 async function InsertData() {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("CONNECTION STARTED")
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("CONNECTION STARTED");
 
-        await PRODUCT.deleteMany({})
-        await PRODUCT.insertMany(data)
-        console.log("Data inserted successfully")
+        for (const item of data) {
+            await PRODUCT.findOneAndUpdate({ name: item.name },
+                item, { upsert: true, new: true }
+            );
+        }
 
+        console.log("Data inserted/updated successfully");
     } catch (err) {
-        console.log("ERROR WHILE INSERTING : ", err)
+        console.log("ERROR WHILE INSERTING:", err);
     } finally {
-        mongoose.connection.close()
-        console.log("CONNECTION CLOSED")
+        await mongoose.connection.close();
+        console.log("CONNECTION CLOSED");
     }
-
-
 }
-InsertData()
+
+InsertData();
